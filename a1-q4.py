1dda45b46a99e8c06e38c1145ef783450053e2fa
ankita0090119 <122302611+ankita0090119@users.noreply.github.com>
2023-01-18 11:36:57 +0530

    Create a1-q4.py

---
+++ a1-q4.py
@@ -0,0 +1,11 @@
+low = int(input ("Please, Enter the Lowest Range Value: "))  
+high = int(input ("Please, Enter the Upper Range Value: "))  
+  
+print ("The Prime Numbers in the range are: ")  
+for number in range (low,high + 1):  
+    if number > 1:  
+        for i in range (2, number):  
+            if (number % i) == 0:  
+                break  
+        else:  
+            print (number)  
